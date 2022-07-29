dedef69319d3b6723e42e1a691e1c8441a5716ac
Elizabeth Gonzalez <elizabeth.gc091@gmail.com>
2022-07-29 17:10:54 -0500

    getting product list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import { Navigate } from "react-router-dom";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+       fetch("https://mocki.io/v1/85c8d4d5-62dc-4246-9781-91ada1ff926d")
+           .then(response => response.json())
+           .then(data => {
+               setProducts(data.products)
+           })
+    }, []);
+
+    function createProduct(){
+        alert("create product")
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className='container'>
+            <div className="d-flex">
+                <div className="p-2 flex-grow-1">
+                    <p className="fs-2">Productos especializados</p>
+                </div>
+                <div className="p-2">
+                    <button type="button" className="btn btn-primary btn-lg">Acciones masivas</button>
+                </div>
+                <div className="p-2">
+                    <button type="button" className="btn btn-secondary btn-lg" onClick={createProduct}> + Cargar producto</button>
+                </div>
+            </div>
+
+
+
+            <table className='table'>
+                <thead>
+                <tr>
+                    <th scope='col'>NOMBRE DEL PRODUCTO</th>
+                    <th scope='col'>SKU SIMPLE</th>
+                    <th scope='col'>SKU</th>
+                    <th scope='col'>UPC</th>
+                    <th scope='col'>PRECIO</th>
+                    <th scope='col'>FECHA DE CARGA</th>
+                    <th scope='col'>ESTADO</th>
+                </tr>
+                </thead>
+                <tbody>
+                {products.map((product,key)=>{
+                    return (<tr key={key}>
+                        <td>{product.product_name}</td>
+                        <td>{product.simple_sku}</td>
+                        <td>{product.sku}</td>
+                        <td>{product.upc}</td>
+                        <td>{product.price}</td>
+                        <td>{product.created_at}</td>
+                        <td>{product.enable}</td>
+                    </tr>)
+                })}
+                </tbody>
+            </table>
+        </div>
+
     </div>
   );
 }
---
+++ src/components/createProduct/CreateProduct.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function CreateProduct() {
+    return(
+        <div>
+            Create product
+        </div>
+    )
+
+}
